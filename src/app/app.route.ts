2b4168922f5b9699edf447c63fa583a62def0b64
Edgar Navarro <egadepaso@gmail.com>
2017-11-08 16:42:09 -0600

    Menu Creado

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -8,7 +8,7 @@ import {
 } from "./components/index.paginas";
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' }
