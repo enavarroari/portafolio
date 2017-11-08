53216d83c448e347d1b913fb92e3f50c842fdf2f
Edgar Navarro <egadepaso@gmail.com>
2017-11-08 15:11:22 -0600

    Header, Body y Footer Listo

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+
+}
