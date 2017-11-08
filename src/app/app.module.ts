4f4002df6aecf25331385329b1a783c1456e1f0d
Edgar Navarro <egadepaso@gmail.com>
2017-11-08 17:32:17 -0600

    Info y InformacionService Creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from "@angular/http";
+
 // Rutas
 import { app_routing } from "./app.route";
+
+// Servicios
+import { InformacionService }  from "./services/informacion.service";
 
 // Componentes
 
@@ -25,9 +30,12 @@ import { ItemComponent } from './components/item/item.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
