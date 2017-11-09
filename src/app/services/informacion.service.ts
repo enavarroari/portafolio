fb6721e3a14621d40bb3b573cc903279f68b33d6
Edgar Navarro <egadepaso@gmail.com>
2017-11-09 10:37:46 -0600

    Animaciones

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,11 +5,13 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  cargada:boolean = false;
 
   constructor( public http:Http ) {
     this.http.get("assets/data/info.pagina.json")
       .subscribe( data => {
         console.log(data.json());
+        this.cargada = true;
         this.info = data.json();
       })
 
