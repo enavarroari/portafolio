73ca3bec20a04af6084bf4b61bb10f799bd62bfd
Edgar Navarro <egadepaso@gmail.com>
2017-11-09 11:07:40 -0600

    Servicio Firebases

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,31 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sn: boolean = false;
+  equipo : any =  {};
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobrenosotros();
+
+  }
+
+  public carga_info ( ) {
+
     this.http.get("assets/data/info.pagina.json")
       .subscribe( data => {
-        console.log(data.json());
+        //console.log(data.json());
         this.cargada = true;
         this.info = data.json();
+      })
+
+  }
+  public carga_sobrenosotros (  ) {
+    this.http.get("https://portafolioweb-f4219.firebaseio.com/equipo.json")
+      .subscribe( data => {
+        console.log(data.json());
+        this.cargada_sn = true;
+        this.equipo = data.json();
       })
 
   }
