63f020655f83cb3287d888c7b40b2c1e08e69d13
Edgar Navarro <egadepaso@gmail.com>
2017-11-09 18:11:13 -0600

    Search Completo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor( private http:Http ) {
@@ -12,16 +13,51 @@ export class ProductosService {
 
   }
 
-  public cargar_productos() {
+  public buscar_producto( termino:string ) {
+    console.log("Buscando Producto");
+    console.log(this.productos.length);
+
     if( this.productos.length === 0 ){
+      this.cargar_productos().then(() => {
+        //Termino la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+
+
+  }
+
+  private filtrar_productos( termino:string ){
+
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+       if( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >=0 ){
+         this.productos_filtrado.push( prod );
+
+         console.log( prod );
+       }
+
+    });
+  }
+
+  public cargar_productos() {
+    let promesa = new Promise (( resolve, rejevct ) => {
       this.cargando = true;
       this.http.get("https://portafolioweb-f4219.firebaseio.com/productos_idx.json")
         .subscribe( res => {
             //console.log( res.json() );
             this.productos = res.json();
             this.cargando = false;
-        })
-    }
+            resolve();
+        });
+    });
+
+    return promesa;
   }
 
   public cargar_producto( cod:string ){
